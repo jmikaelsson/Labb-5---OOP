f89e5f5d6d2d63200890874f392759d57bf2cbc5
Josefin Mikaelsson <josefin.mikaelsson.NET23@edu.edugrade.se>
2023-10-12 13:56:03 +0200

    Add project files.

---
+++ Circel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_5___OOP
+{
+    internal class Circel
+    {
+        public float _pi;
+        public int _radius;
+        public float _area;
+        public float _circumference;
+        public float _volym;
+
+        public Circel(int radius)
+        {
+            _pi = 3.141f;
+            _radius = radius;
+            _area = radius*radius*_pi;
+            _circumference = _pi * 2 * radius;
+            _volym = (4 * _pi * (radius * radius * radius)) / 3;
+        }
+        public void GetArea()
+        {
+           Console.WriteLine(_area + " cm²");
+        }
+
+        public void GetCircumference()
+        {
+            Console.WriteLine(_circumference + " cm");
+        }
+
+        public void GetVolym ()
+        {
+            Console.WriteLine(_volym + "cm³");
+            
+        }
+    }
+}
---
+++ Triangel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_5___OOP
+{
+    internal class Triangel
+    {
+        public float _area;
+        public float _circumference;
+        public float _volym;
+        public int _base;
+        public int _hight;
+
+        public Triangel(int triangelBase, int triangelHight)
+        {
+            _area = (triangelBase * triangelHight) / 2;
+            //_circumference = ;
+            //_volym = ;
+
+
+        }
+
+        public void GetArea()
+        {
+            Console.WriteLine(_area + " cm²");
+        }
+
+        //public void GetCircumference()
+        //{
+        //    Console.WriteLine(_circumference + " cm");
+        //}
+
+        //public void GetVolym()
+        //{
+        //    Console.WriteLine(_volym + "cm³");
+        //}
+
+    }
+
+
+}
